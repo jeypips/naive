45d2d63175c6dbf3fa86e43a56110d4a64b15b20
Sly Flores <sly@christian.com.ph>
2018-08-28 15:14:59 +0800

    added real lgu no

--- handlers/cmcis/cmcis.php
+++ handlers/cmcis/cmcis.php
@@ -6,9 +6,9 @@ require_once '../../db.php';
 
 $con = new pdo_db();
 
-$cmcis = $con->getData("SELECT *, DATE_FORMAT(system_log, '%M %d, %Y') system_log FROM cmci");
+$cmcis = $con->getData("SELECT cmci.id, lgus.lgu_no, cmci.lgu_id, cmci.period_covered, DATE_FORMAT(cmci.system_log, '%M %d, %Y') date_added FROM cmci LEFT JOIN lgus ON cmci.lgu_id = lgus.id");
 
-foreach ($cmcis as $key => $cmci){
+foreach ($cmcis as $key => $cmci) {
 	
 	$lgu = $con->getData("SELECT lgus.id, (SELECT municipalities.municipality_description FROM municipalities WHERE municipalities.municipality_id = lgus.municipality) municipality FROM lgus WHERE lgus.id = ".$cmci['lgu_id']);	
 	$cmcis[$key]['lgu_id'] = $lgu[0];
