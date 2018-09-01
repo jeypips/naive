b6bc20cf3604cfb7b0ef3b0265592225acba9ea1
Jp <johnpaulbalanon6@gmail.com>
2018-09-01 21:22:37 +0800

    new title PDCI & predictions

---
+++ controllers/prediction.js
@@ -0,0 +1,11 @@
+var app = angular.module('prediction',['account-module','app-module']);
+
+app.controller('predictionCtrl',function($scope,app) {
+
+	$scope.app = app;
+
+	app.data($scope);
+	app.list($scope);
+		
+});
+
